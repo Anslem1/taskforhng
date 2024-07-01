1f9cf311bc6c6161a07c00566fd08989d7ba9be1
Anslem1 <andrewanslem1@gmail.com>
2024-07-01 14:47:52 +0100

    vercel

---
+++ api/index.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	. "github.com/tbxark/g4vercel"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	server := New()
+
+	query := r.URL.Query().Get("visitor_name")
+	ipAddr := "127.0.0.1"
+	query = strings.Trim(query, `"`)
+
+	greeting := fmt.Sprintf("Hello, %v! The temperature is 11 degrees Celsius in Mark.", query)
+
+	server.GET("/api/hello", func(context *Context) {
+		context.JSON(200, H{
+			"client_ip": ipAddr,
+			"greeting":  greeting,
+			"location":  "New York",
+		})
+	})
+	server.Handle(w, r)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Anslem1/taskforhng
 
 go 1.22.1
+
+require github.com/tbxark/g4vercel v0.0.4
