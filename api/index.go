d62ebc0607fcc84e7989e3602084c1dcc07737b8
Anslem1 <andrewanslem1@gmail.com>
2024-07-01 16:19:42 +0100

    pose to work?

--- api/index.go
+++ api/index.go
@@ -1,92 +1,85 @@
 package handler
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-
+	"net"
 	"net/http"
 	"os"
 
+	"github.com/ipinfo/go/ipinfo"
 	"github.com/joho/godotenv"
 	. "github.com/tbxark/g4vercel"
 )
 
 type ipInfo struct {
-    City    string `json:"city"`
-    Region  string `json:"region"`
-    Country string `json:"country"`
+	City    string `json:"city"`
+	Region  string `json:"region"`
+	Country string `json:"country"`
 }
 
 func getClientIP(r *http.Request) string {
-    ip := r.Header.Get("X-Real-IP")
-    if ip == "" {
-        ip = r.Header.Get("X-Forwarded-For")
-    }
-    if ip == "" {
-        ip = r.RemoteAddr
-    }
-    return ip
+	ip := r.Header.Get("X-Real-IP")
+	if ip == "" {
+		ip = r.Header.Get("X-Forwarded-For")
+	}
+	if ip == "" {
+		ip, _, _ = net.SplitHostPort(r.RemoteAddr)
+	}
+	return ip
 }
 
-func getCityInfo(ip string) (ipInfo, error) {
-    var info ipInfo
+func getCityInfo(ip string) (string, error) {
+	var info ipInfo
 
-    // Load environment variables from .env file
-    if err := godotenv.Load(); err != nil {
-        return info, fmt.Errorf("error loading .env file")
-    }
+	// Load environment variables from .env file
+	if err := godotenv.Load(); err != nil {
+		return "", fmt.Errorf("error loading .env file")
+	}
 
-    apiToken := os.Getenv("IPINFO_API_TOKEN")
-    if apiToken == "" {
-        return info, fmt.Errorf("IPINFO_API_TOKEN is not set")
-    }
+	apiToken := os.Getenv("IPINFO_API_TOKEN")
+	if apiToken == "" {
+		return "", fmt.Errorf("IPINFO_API_TOKEN is not set")
+	}
 
-    url := fmt.Sprintf("https://ipinfo.io/%s?token=%s", ip, apiToken)
-    resp, err := http.Get(url)
-    if err != nil {
-        return info, err
-    }
-    defer resp.Body.Close()
+	client := ipinfo.NewClient(http.DefaultClient)
+	netIp := net.ParseIP(ip)
+	ipInfoResp, err := client.GetCity(netIp)
+	if err != nil {
+		return "", err
+	}
 
-    body, err := io.ReadAll(resp.Body)
-    if err != nil {
-        return info, err
-    }
-    if err := json.Unmarshal(body, &info); err != nil {
-        return info, err
-    }
-    return info, nil
+	ipInfoResp = info.City 
+	return ipInfoResp , nil
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-    server := New()
+	server := New()
 
-    server.GET("/", func(context *Context) {
-        context.JSON(200, H{
-            "message": "hello go from hng !!!!",
-        })
-    })
+	server.GET("/", func(context *Context) {
+		context.JSON(200, H{
+			"message": "hello go from hng !!!!",
+		})
+	})
 
-    server.GET("/api/hello", func(context *Context) {
-        name := context.Query("visitor_name")
-        ip := getClientIP(r)
-        cityInfo, err := getCityInfo(ip)
+	server.GET("/api/hello", func(context *Context) {
+		name := context.Query("visitor_name")
+		ip := getClientIP(r)
+		cityInfo, err := getCityInfo(ip)
 
-        if err != nil || name == "" {
-            context.JSON(400, H{
-                "message":   "name not found or error getting city info",
-                "client_ip": ip,
-                "location":  cityInfo.City,
-            })
-        } else {
-            context.JSON(200, H{
-                "message":   fmt.Sprintf("Hello, %v! The temperature is 11 degrees Celsius in %v.", name, cityInfo.City),
-                "client_ip": ip,
-                "location":  cityInfo.City,
-            })
-        }
-    })
+		if err != nil || name == "" {
+			context.JSON(400, H{
+				"message":   "name not found or error getting city info",
+				"client_ip": ip,
+				"location":  cityInfo,
+			})
+		} else {
+			context.JSON(200, H{
+				"message":   fmt.Sprintf("Hello, %v! The temperature is 11 degrees Celsius in %v.", name, cityInfo),
+				"client_ip": ip,
+				"location":  cityInfo,
+			})
+		}
+	})
 
-    server.Handle(w, r)
+	server.Handle(w, r)
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,12 @@ go 1.22.1
 
 require github.com/tbxark/g4vercel v0.0.4
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require github.com/ipinfo/go-ipinfo v1.0.0 // indirect
+
+require (
+	github.com/ipinfo/go v1.0.0
+	github.com/ipinfo/go/v2 v2.10.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	golang.org/x/sync v0.0.0-20220513210516-0976fa681c29 // indirect
+)
